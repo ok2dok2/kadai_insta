9c2cbda917405d01de68080c23a48a06c567c15f
ok2dok2 <lkim.pcr@gmail.com>
2023-06-13 14:12:16 +0900

    photo controller基本アクション実装完了

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,6 @@
 class PhotosController < ApplicationController
   def index
-    
+    @photos =Photo.all
   end
   
   def new
@@ -10,17 +10,38 @@ class PhotosController < ApplicationController
   def create
     @photo = Photo.new(photo_params)
     if @photo.save
-      redirect_to photos_path
+      redirect_to photos_path #index
     else
       render new
     end
-    
   end
 
+  def show
+    @photo = Photo.find(params[:id])
+  end
+
+  def edit
+    @photo = Photo.find(params[:id])
+  end
+  
+  def update
+    @photo = Photo.find(params[:id])
+    if @photo.update(photo_params)
+      redirect_to photos_path,notice: "更新しました"
+    else
+      render :new
+    end
+  end
+  
+  def destroy
+    @photo = Photo.find(params[:id])
+    @photo.destroy
+    redirect_to photos_path
+  end
 
   private
-
+  
   def photo_params
-    params.require(:photo).parmit([:content, :image])
+    params.require(:photo).permit(:content, :image, :image_cache)
   end
 end
